59818e30b7b9a03469e7f72d450d2f0eee23df44
ParthAggarwal1404 <141123480+ParthAggarwal1404@users.noreply.github.com>
2023-08-23 17:42:36 +0530

    Add files via upload

---
+++ Counting_total_number_of_char_in_java.java
@@ -0,0 +1,17 @@
+public class Count_Char  
+{    
+    public static void main(String[] args) {    
+        String string = "We are learning Java";    
+        int count = 0;    
+            
+        //Counts each character except space
+    
+        for(int i = 0; i < string.length(); i++) {    
+            if(string.charAt(i) != ' ')    
+                count++;    
+        }    
+            
+        //Displays the total number of characters present in the given string    
+        System.out.println("Total number of characters in a string: " + count);    
+    }    
+} 
