33e49cd8850a9d2653a40ef2b024b3eeaaf2e286
ParthAggarwal1404 <141123480+ParthAggarwal1404@users.noreply.github.com>
2023-08-23 18:25:08 +0530

    Add files via upload

---
+++ Duplicate_number.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Duplicate_number {
+    public static void main(String[] args) {
+        final int SIZE = 10;
+        int[] numbers = new int[SIZE];
+        int num, count;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter ten numbers between 10 and 100: ");
+
+        for (int i = 0; i < SIZE; i++) {
+            do {
+                count = 0;
+                num = input.nextInt();
+                if (num < 10 || num > 100) {
+                    System.out.println("Error: number must be between 10 and 100. Please enter again: ");
+                } else {
+                    for (int j = 0; j < i; j++) {
+                        if (numbers[j] == num) {
+                            count++;
+                        }
+                    }
+                    if (count == 0) {
+                        numbers[i] = num;
+                        for (int k = 0; k <= i; k++) {
+                            System.out.print(numbers[k] + " ");
+                        }
+                        System.out.println();
+                    } else {
+                        System.out.println("Error: number is a duplicate. Please enter again: ");
+                    }
+                }
+            } while (count != 0);
+            
+        }
+    }
+}
